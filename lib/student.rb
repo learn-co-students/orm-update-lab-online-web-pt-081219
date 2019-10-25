077368ca427e3c9bb9c2af48355f0cf5f1b26fc6
Joseph Sesti <sestijoey63@gmail.com>
2019-10-25 20:39:14 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -30,7 +30,6 @@ class Student
   end
   
   def save
-    # binding.pry
     if self.id
       update self.id
     else
@@ -39,18 +38,34 @@ class Student
         VALUES (?,?)
       SQL
     
-      DB[:conn].execute(sql, self,name, self.grade)
+      DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
   
-  # def self.create(name, grade)
-  #   student = Student.new(name, grade)
-  #   student.save
-  #   student
-  # end
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
   
-  # def self.new_from_db(array)
-    
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end
+  
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ? LIMIT 1"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
+  end
+  
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
   
 end
